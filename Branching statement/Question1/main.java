ab16d780273ce946903a1b4d226c79e9942e1fc8
ZZCWC <85448514+losier@users.noreply.github.com>
2022-08-04 19:56:23 +0530

    added README

--- Branching statement/Question1/main.java
+++ Branching statement/Question1/main.java
@@ -3,29 +3,31 @@ import java.util.Scanner;
 public class main {
 
     static void SquareAndCude() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        float number = input.nextFloat();
-        System.out.println("Square of " + number + " is " + (number * number));
-        System.out.println("Cube of " + number + " is " + (number * number * number));
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Enter a number: ");
+            float number = input.nextFloat();
+            System.out.println("Square of " + number + " is " + (number * number));
+            System.out.println("Cube of " + number + " is " + (number * number * number));
+        }
     }
 
     static void Leap() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a year: ");
-        int year = input.nextInt();
-        if (year % 4 == 0) {
-            if (year % 100 == 0) {
-                if (year % 400 == 0) {
-                    System.out.println(year + " is a leap year");
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Enter a year: ");
+            int year = input.nextInt();
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        System.out.println(year + " is a leap year");
+                    } else {
+                        System.out.println(year + " is not a leap year");
+                    }
                 } else {
-                    System.out.println(year + " is not a leap year");
+                    System.out.println(year + " is a leap year");
                 }
             } else {
-                System.out.println(year + " is a leap year");
+                System.out.println(year + " is not a leap year");
             }
-        } else {
-            System.out.println(year + " is not a leap year");
         }
     }
 
@@ -36,8 +38,9 @@ public class main {
         System.out.println("1. Square and cube of a number\n");
         System.out.println("2. Leap year\n");
         System.out.println("3. Exit\n");
-        Scanner input = new Scanner(System.in);
-        choice = input.nextInt();
+        try (Scanner input = new Scanner(System.in)) {
+            choice = input.nextInt();
+        }
         if (choice == 1) {
             SquareAndCude();
         } else if (choice == 2) {
